a7a0ee0aacd83a3a4a7c67a33eea7813a9f85ad6
shiv <shivjeet@capiot.com>
2019-02-12 14:16:15 +0530

    added a few more variations to rotational and scaling transformations

--- augs/functions.py
+++ augs/functions.py
@@ -15,7 +15,11 @@ def mirror(img):
 def rotate(img):
     im_neg = img.rotate(-10)
     im_pos = img.rotate(10)
-    return [(im_neg, "_neg10"), (im_pos, "_pos10")]
+
+    im_neg15 = img.rotate(-15)
+    im_pos15 = img.rotate(15)
+    return [(im_neg, "_neg10"), (im_pos, "_pos10"),
+            (im_neg15, "_neg15"), (im_pos15, "_pos15")]
 
 def scale(img):
     h,w = img.size
@@ -24,7 +28,16 @@ def scale(img):
     im_up = (int(h/factor), int(w/factor))
     im_ups = img.resize(im_up)
     im_downs = img.resize(im_down)
-    return [(im_ups, "_upsamp"), (im_downs, "_downsamp")]
+
+    # further scaling
+    factor = .60
+    im_down2 = (int(h*factor), int(w*factor))
+    im_up2 = (int(h/factor), int(w/factor))
+    im_ups2 = img.resize(im_up2)
+    im_downs2 = img.resize(im_down2)
+
+    return [(im_ups, "_upsamp"), (im_downs, "_downsamp"),
+            (im_ups2, "_upsamp6"), (im_downs2, "_downsamp6")]
 
 def noise(img):
     im2 = img.filter(ImageFilter.MedianFilter)
